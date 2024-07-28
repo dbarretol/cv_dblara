a2ab33b0a77fe85cf4632b6823102f973bf529a5
dbarretol <57958754+dbarretol@users.noreply.github.com>
2024-07-27 19:02:28 -0500

    archivos base

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:"https://dbarretol.github.io/cv_dblara"
+})
